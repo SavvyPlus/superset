a8674731547002b5064fbcadde156aa5a45b55c7
Kev Jackson <foamdino@gmail.com>
2018-06-05 17:42:10 +0100

    continue building kops docker

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ and install [superset](https://superset.apache.org/)
 Checkout the repo from github and:
 1. `cd superset-cluster/k8s`
 1. `./cluster.sh`
+1. From inside the superset-cluster-kops docker container, run the following to create the k8s spec
+`kops create cluster --node-size=t2.large --zones=ap-southeast-2a,ap-southeast-2b --node-count=2 --name=${NAME}`
+1. From inside the superset-cluster-kops docker container, run the following to use the spec to create the k8s cluster
+`kops update cluster ${NAME} --yes`
--- k8s/cluster.sh
+++ k8s/cluster.sh
@@ -1,10 +1,31 @@
 # run this to create a k8s cluster in AWS
 
-echo "Please ensure that you have already configured DNS: https://github.com/kubernetes/kops/blob/master/docs/aws.md"
+CLUSTERNAME=$1
+if [ -z "${CLUSTERNAME:+x}" ]; then
+  echo "CLUSTERNAME not set, using superset.savvybi.enterprises";
+  CLUSTERNAME=superset.savvybi.enterprises
+else
+  echo "CLUSTERNAME set to '$CLUSTERNAME'";
+fi
 
-echo "Please ensure that your aws account is configured with enough permissions to correctly create the kops user"
+echo "Please ensure that you have already configured DNS: https://github.com/kubernetes/kops/blob/master/docs/aws.md\n"
+read -p "Press any key to continue... \n" -n1 -s
+
+echo "Please ensure that your aws account is configured with enough permissions to correctly create the kops user\n"
+read -p "Press any key to continue... \n" -n1 -s
+
+echo "Creating s3 bucket for cluster store..."
+aws s3api create-bucket --bucket savvybi-superset-state-store --region us-east-1
+aws s3api put-bucket-versioning --bucket savvybi-superset-state-store  --versioning-configuration Status=Enabled
 
 cp ~/.aws/credentials credentials
+cp ~/.ssh/id_rsa.pub id_rsa.pub
 
 echo "Building docker image to run cluster commands - this will take some time..."
-sudo docker build . -t savvybi/superset-cluster:0.1  
+echo "clustername: ${CLUSTERNAME}"
+sudo docker build . -t savvybi/superset-cluster-kops:0.1 --build-arg CLUSTERNAME=${CLUSTERNAME}
+
+echo "Running cluster commands in new container..."
+export KOPS_STATE_STORE=s3://savvybi-superset-state-store
+export NAME=$CLUSTERNAME
+sudo docker run savvybi/superset-cluster-kops:0.1
