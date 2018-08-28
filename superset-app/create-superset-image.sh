49a48080a1884b64ffe9c1701ffd36ea3e622d47
Kev Jackson <foamdino@gmail.com>
2018-08-28 17:15:14 +0100

    tweak docker image creation to auto-increment version number and deploy production tag to cluster

--- superset-app/create-superset-image.sh
+++ superset-app/create-superset-image.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-VERSION=$1
-if [ -z "${VERSION:+x}" ]; then
-  echo "VERSION not set, using 0.1-dev";
-  VERSION=0.1-dev
+LATEST_VERSION=$(sudo docker images --format "{{.Tag}}" savvybi/superset-app | sed 's/-dev$//')
+echo $LATEST_VERSION
+if [ -z "${LATEST_VERSION:+x}" ]; then
+  echo "VERSION not set, using 1-dev";
+  VERSION=1-dev
 else
+  VERSION=$((LATEST_VERSION+1))
   echo "VERSION set to '$VERSION'";
 fi
 
@@ -13,7 +15,7 @@ echo "Using account: ${AWS_ACCOUNT_ID}"
 DOCKER_LOGIN=$(aws ecr get-login --no-include-email --region=ap-southeast-2)
 sudo $DOCKER_LOGIN
 
-sudo docker build --build-arg VER=${VERSION} --rm . -t savvybi/superset-app:${VERSION} -t ${AWS_ACCOUNT_ID}.dkr.ecr.ap-southeast-2.amazonaws.com/savvybi/superset-app:${VERSION}
+sudo docker build --build-arg VER=${VERSION} --rm . -t savvybi/superset-app:${VERSION} -t ${AWS_ACCOUNT_ID}.dkr.ecr.ap-southeast-2.amazonaws.com/savvybi/superset-app:${VERSION} -t ${AWS_ACCOUNT_ID}.dkr.ecr.ap-southeast-2.amazonaws.com/savvybi/superset-app:production
 
 echo "Creating ECR repository: savvybi/superset-app"
 aws ecr create-repository --region=ap-southeast-2 --repository-name savvybi/superset-app ||:
