7bc0ff2a4a0d440a70d6f55a4f2c375bdbfd5640
Kev Jackson <foamdino@gmail.com>
2018-10-27 07:55:46 +0100

    correct kops state store to use dts/prod correctly

--- k8s/env-dts.sh
+++ k8s/env-dts.sh
@@ -1,4 +1,4 @@
 # Run this in the docker container to export correct env to kubecfg
-export KOPS_STATE_STORE=s3://savvybi-superset-prod-state-store
-export NAME=aws2-vpc-ss-prod.savvybi.enterprises
+export KOPS_STATE_STORE=s3://savvybi-superset-dts-state-store
+export NAME=aws2-vpc-ss-dts.savvybi.enterprises
 kops export kubecfg --name=${NAME}
