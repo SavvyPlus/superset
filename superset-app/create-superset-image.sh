5b0085187ce4fd62f9b45d63e0a57e59e91b935f
Marko Knezevic <SAVVYPLUS\marko.knezevic@a-2tdegpfe35yar.savvyplus.local>
2019-01-30 19:10:26 +0100

    SolarBI Dockerfile and Kubernetes

--- superset-app/create-superset-image.sh
+++ superset-app/create-superset-image.sh
@@ -12,14 +12,14 @@ fi
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
 echo "Using account: ${AWS_ACCOUNT_ID}"
 
-DOCKER_LOGIN=$(aws ecr get-login --no-include-email --region=us-west-2)
+DOCKER_LOGIN=$(aws ecr get-login --no-include-email --region=ap-southeast-2)
 sudo $DOCKER_LOGIN
 
 echo "Docker Build"
-sudo docker build --build-arg VER=${VERSION} --rm . -t zawee/superset-app:${VERSION} -t ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/zawee/superset-app:${VERSION} -t ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/zawee/superset-app:production
+sudo docker build --build-arg VER=${VERSION} --rm . -t zawee/superset-app:${VERSION} -t ${AWS_ACCOUNT_ID}.dkr.ecr.ap-southeast-2.amazonaws.com/zawee/superset-app:${VERSION} -t ${AWS_ACCOUNT_ID}.dkr.ecr.ap-southeast-2.amazonaws.com/zawee/superset-app:production
 
 echo "Creating ECR repository: zawee/superset-app"
-aws ecr create-repository --region=us-west-2 --repository-name zawee/superset-app ||:
+aws ecr create-repository --region=ap-southeast-2 --repository-name zawee/superset-app ||:
 
 echo "Pushing image to ECR"
-sudo docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/zawee/superset-app
+sudo docker push ${AWS_ACCOUNT_ID}.dkr.ecr.ap-southeast-2.amazonaws.com/zawee/superset-app
